c1d0ef2bdd00fd2f53372bce7c90a69d9dc616fa
VINH DO <vinhdo0913@gmail.com>
2021-10-23 09:02:41 +0700

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import BlogListView, BlogDetailView
+
+urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post-detail'),  # new
+    path('', BlogListView.as_view(),name='home'),
+]
