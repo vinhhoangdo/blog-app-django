1a6a62657710059db29b661af7e03508fedaa3a1
VINH DO <vinhdo0913@gmail.com>
2021-10-24 09:32:09 +0700

    forms and user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'blog',  # new 
+    'blog',
+    'accounts',  # new 
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home' # new
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # new
+    path('account/', include('django.contrib.auth.urls')),
+    path('', include('blog.urls')),
+    path('accounts/',include('accounts.urls')),  # new
 ]
